fe324a586fcf4844aa221b11768b047b47c7daa3
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-10-05 17:16:02 +0200

    Listázás gomb

--- main.js
+++ main.js
@@ -23,6 +23,28 @@ function hozzaadas() {
     filmNev = "";
     filmRendezo = "";
     filmKiadasiEve  = "";
+    
 }
 
 document.getElementById("hozza").addEventListener("click", hozzaadas);
+
+
+
+function listazas() {
+    let tablazat = "<table>";
+    for (let i = 0; i < tomb.length; i++) {
+        tablazat += "<tr>";
+        tablazat += "<td>" + tomb[i].nev + "</td>";
+        tablazat += "<td>" + tomb[i].rendezo + "</td>";
+        tablazat += "<td>" + tomb[i].kiadasiEv + "</td>";
+        tablazat += "</tr>";
+        
+    }
+    tablazat += "</table>";
+    document.getElementById("tabla").innerHTML = tablazat;
+    
+    
+
+}
+
+document.getElementById("listaz").addEventListener("click", listazas);
