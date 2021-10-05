a0f1f66cc493c3ffe5cf3c7023a7ceb97714f938
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-10-05 14:08:33 +0200

    Űrlap létrehozása a bemeneti mezőkkel

---
+++ main.js
@@ -0,0 +1,13 @@
+class Film {
+    nev;
+    rendezo;
+    kiadasiEv;
+    tomb;
+
+    constructor(nev, rendezo, kiadasEve) {
+        this.nev = nev;
+        this.rendezo = rendezo;
+        this.kiadasiEv = kiadasEve;
+        this.tomb = [];
+    }
+}
